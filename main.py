8494015e8a2ae8cf690f1c4902c191c35aff8bb0
Ahmed Khaled <80486237+Ahmed-Khaled24@users.noreply.github.com>
2022-06-14 22:38:55 +0200

    refactor: add constants
    add constants to make the code easiear to edit, and reformat some
    comments.

--- main.py
+++ main.py
@@ -1,40 +1,45 @@
 from turtle import Screen
-from snake import Snake
-from food import Food
-from scoreboard import Scoreboard
+from snake import *
+from food import *
+from scoreboard import *
 import time
 
-# initialize the screen
+# Constants
+SCREEN_WIDTH = 600
+SCREEN_HEIGHT = 600
+SCREEN_COLOR = "black"
+SCREEN_TITLE = "Snake Game"
+
+# Initialize the screen.
 screen = Screen()
-screen.setup(width=600, height=600)
-screen.bgcolor("black")
-screen.title("Snake Game")
+screen.setup(width=SCREEN_WIDTH, height=SCREEN_HEIGHT)
+screen.bgcolor(SCREEN_COLOR)
+screen.title(SCREEN_TITLE)
 screen.tracer(0)
 
-# initialize the snake
+# Initialize snake body and food object.
 snake = Snake()
 food = Food()
 
-# initialize score and highest score boards
-score_bd = Scoreboard()
-score_bd.score_position()
-score_bd.update_score()
+# Initialize score and highest score boards.
+score_board = Scoreboard()
+score_board.goto(SCORE_POSITION)
+score_board.update_score()
+highest_score_board = Scoreboard()
+highest_score_board.goto(HIGHEST_SCORE_POSITION)
 
-highest_score_bd = Scoreboard()
-highest_score_bd.highest_score_position()
-
-# check if the file exist before if it doesn't then create it.
+# Check if the file exist before. If it doesn't then create it.
 try:
-    highest_score_bd.get_highest_score()
+    highest_score_board.get_highest_score()
 except FileNotFoundError:
     print("Creating highest-score.txt file")
     file = open(file="highest-score.txt", mode="w")
     file.write("0")
     file.close()
 
-highest_score_bd.update_highest_score()
+highest_score_board.update_highest_score()
 
-# set control keys
+# Set control keys
 screen.listen()
 screen.onkey(key="Up", fun=snake.up)
 screen.onkey(key="Right", fun=snake.right)
@@ -44,29 +49,29 @@ screen.onkey(key="Down", fun=snake.down)
 game_on = True
 while game_on:
     screen.update()
-    snake.move()  # move the snake forward
+    snake.move()
     screen.update()
     time.sleep(0.05)
 
-    # Detect collision with food and track the score
+    # Detect collision with food and track the score.
     if snake.head.distance(food) <= 15:
         food.refresh()
         snake.extend()
-        score_bd.score_up()
-        if score_bd.score > highest_score_bd.score:
-            highest_score_bd.score = score_bd.score
-            highest_score_bd.update_highest_score()
+        score_board.score_up()
+        if score_board.score > highest_score_board.score:
+            highest_score_board.score = score_board.score
+            highest_score_board.update_highest_score()
 
-    # Detect collision with walls
+    # Detect collision with walls.
     if snake.head.xcor() >= 280 or snake.head.xcor() <= -280 or snake.head.ycor() >= 280 or snake.head.ycor() <= -280:
         game_on = False
-        score_bd.game_over()
+        score_board.game_over()
 
-    # Detect collision with snake body
+    # Detect collision with snake body.
     for segment in snake.segments:
         if snake.head.distance(segment) < 5 and segment != snake.head:
             game_on = False
-            score_bd.game_over()
+            score_board.game_over()
 
-highest_score_bd.save_highest_score()
+highest_score_board.save_highest_score()
 screen.exitonclick()
